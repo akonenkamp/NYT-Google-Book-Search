eef456ad8925bd00e3f83669606c8d3752d44dc0
Konenkamp <n0267144@lm.lmig.com>
2019-03-13 19:37:50 -0700

    appjs added

--- client/src/app.js
+++ client/src/app.js
@@ -4,3 +4,22 @@ import Home from "./pages/Home";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <Nav />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/saved" component={Saved} />
+                    <Route component = {NoMatch} />
+                </Switch>
+            </div>
+        </Router>
+
+    );
+}
+
+export default App;
